9e2d6ee2f6a473bf49a1e310cc2956ca9eb99f13
Smaaihllaamss <smaajob@proton.me>
2024-11-10 20:19:50 +0200

    changed the header

--- js/header.js
+++
@@ -1,9 +0,0 @@
-/* Смена фона header при скролинге на Главной */
-
-const header = document.getElementById("header");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else header.classList.remove("scrolled");
-});
